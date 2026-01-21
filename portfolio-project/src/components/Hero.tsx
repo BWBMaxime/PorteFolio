a406a5c4e0dc49b27d1436a9201437041cf41267
LemonBlanc <83752129+BWBMaxime@users.noreply.github.com>
2026-01-21 13:11:17 +0100

    Update dispo

--- portfolio-project/src/components/Hero.tsx
+++ portfolio-project/src/components/Hero.tsx
@@ -51,7 +51,7 @@ export default function Hero() {
                 <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
                 <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
               </span>
-              Disponible sur Montpellier & Remote
+              Disponible immédiatement sur Montpellier
             </div>
           </motion.div>
 
